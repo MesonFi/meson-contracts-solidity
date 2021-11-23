855c5f248386b6afe996b735569eb7b7b33c189e
Phil Li <qft.gtr@gmail.com>
2021-11-23 11:35:44 +0800

    clean fixture

--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -1,33 +1,8 @@
 import { ethers } from 'hardhat'
-// import { Contract, Wallet } from 'ethers'
-
-// import { expandTo18Decimals } from './utilities'
-
-// import ERC20 from '../../build/ERC20.json'
 
 import { MockToken } from '../../typechain/MockToken'
 import { MesonPoolsTest } from '../../typechain/MesonPoolsTest'
 
-// interface FactoryFixture {
-//   factory: 
-// }
-
-// const overrides = {
-//   gasLimit: 9999999
-// }
-
-// export async function factoryFixture(_: Web3Provider, [wallet]: Wallet[]): Promise<FactoryFixture> {
-//   const factory = await deployContract(wallet, UniswapV2Factory, [wallet.address], overrides)
-//   return { factory }
-// }
-
-// interface PairFixture extends FactoryFixture {
-//   token0: Contract
-//   token1: Contract
-//   pair: Contract
-// }
-
-// export async function pairFixture(): Promise<PairFixture> {
 export async function fixtures() {
   const tokenFactory = await ethers.getContractFactory('MockToken')
   const token1: MockToken = await tokenFactory.deploy(1000000000)
