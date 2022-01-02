df8dc975d2db0962834a0862cb7140c0be4f9de0
Phil Li <qft.gtr@gmail.com>
2022-01-02 13:27:55 -0800

    separate user & lp clients

--- test/MesonSwap.spec.ts
+++ test/MesonSwap.spec.ts
@@ -8,60 +8,62 @@ import { fixtures, TOKEN_BALANCE } from './shared/fixtures'
 import { getDefaultSwap } from './shared/meson'
 
 describe('MesonSwap', () => {
-  let mesonInstance: MesonSwapTest
   let token: MockToken
   let unsupportedToken: MockToken
+  let mesonInstance: MesonSwapTest
   let outChain: string
-  let mesonClient: MesonClient
+  let userClient: MesonClient
+  let lpClient: MesonClient
 
   beforeEach('deploy MesonSwapTest', async () => {
     const result = await waffle.loadFixture(() => fixtures([
       initiator.address, provider.address
     ]))
-    mesonInstance = result.swap.connect(provider)
     token = result.token1.connect(initiator)
     unsupportedToken = result.token2.connect(initiator)
+    mesonInstance = result.swap.connect(provider)
 
     outChain = await mesonInstance.getCoinType()
-    mesonClient = await MesonClient.Create(mesonInstance)
+    userClient = await MesonClient.Create(result.swap)
+    lpClient = await MesonClient.Create(mesonInstance)
   })
 
 
   describe('#postSwap', () => {
     it('posts a swap', async () => {
-      const swap = mesonClient.requestSwap(outChain, getDefaultSwap({ inToken: token.address }))
+      const swap = userClient.requestSwap(outChain, getDefaultSwap({ inToken: token.address }))
       const exported = await swap.exportRequest(initiator)
 
       const signedRequest = new SignedSwapRequest(exported)
       await token.approve(mesonInstance.address, swap.amount)
-      await mesonClient.post(signedRequest)
+      await lpClient.post(signedRequest)
 
       expect(await mesonInstance.hasSwap(swap.swapId)).to.equal(true)
       expect(await token.balanceOf(initiator.address)).to.equal(TOKEN_BALANCE.sub(swap.amount))
     })
 
     it('refuses unsupported token', async () => {
-      const swap = mesonClient.requestSwap(outChain, getDefaultSwap({ inToken: unsupportedToken.address }))
+      const swap = userClient.requestSwap(outChain, getDefaultSwap({ inToken: unsupportedToken.address }))
       const exported = await swap.exportRequest(initiator)
 
       const signedRequest = new SignedSwapRequest(exported)
       await unsupportedToken.approve(mesonInstance.address, swap.amount)
-      await expect(mesonClient.post(signedRequest)).to.be.revertedWith('unsupported token')
+      await expect(lpClient.post(signedRequest)).to.be.revertedWith('unsupported token')
     })
   })
 
   describe('#executeSwap', () => {
     it('can execute a swap', async () => {
-      const swap = mesonClient.requestSwap(outChain, getDefaultSwap({ inToken: token.address }))
+      const swap = userClient.requestSwap(outChain, getDefaultSwap({ inToken: token.address }))
       const exported = await swap.exportRequest(initiator)
 
       const signedRequest = new SignedSwapRequest(exported)
       await token.approve(mesonInstance.address, swap.amount)
-      await mesonClient.post(signedRequest)
+      await lpClient.post(signedRequest)
 
       const signedRelease = await swap.exportRelease(initiator)
       SignedSwapRequest.CheckReleaseSignature(signedRelease)
-      await mesonClient.execute(signedRelease)
+      await lpClient.execute(signedRelease)
 
       expect(await mesonInstance.hasSwap(swap.swapId)).to.equal(false)
       expect(await token.balanceOf(initiator.address)).to.equal(TOKEN_BALANCE.sub(swap.amount))
