350d195885c22d93d44dce4df4dd8bcbd8659219
Phil Li <qft.gtr@gmail.com>
2022-01-03 11:41:13 -0800

    update preset networks

--- packages/presets/src/index.ts
+++ packages/presets/src/index.ts
@@ -1,29 +1,6 @@
-import { Contract } from 'ethers'
-import { MesonClient } from '@meson/sdk'
-import { Meson } from '@meson/contract-abis'
+import PresetClients from './PresetClients'
 
-import presets from './presets.json'
+export { default as mainnets } from './mainnets.json'
+export { default as testnets } from './testnets.json'
 
-class Presets {
-  private _cache: Map<string, MesonClient>
-
-  constructor () {
-    this._cache = new Map()
-  }
-
-  getClient (id) {
-    const match = presets.find(item => item.id === id)
-    if (!match) {
-      console.warn(`Unsupported network: ${id}`)
-      return
-    }
-    if (!this._cache.get(id)) {
-      const instance = new Contract(match.mesonAddress, Meson.abi)
-      const client = new MesonClient(instance, Number(match.chainId), match.slip44)
-      this._cache.set(id, client)
-    }
-    return this._cache.get(id)
-  }
-}
-
-export default new Presets()
+export default new PresetClients()
