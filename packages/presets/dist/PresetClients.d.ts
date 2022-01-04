700c18aa9b2621c717194f5d1f7a52e03a8db5df
Phil Li <qft.gtr@gmail.com>
2022-01-03 20:57:31 -0800

    rm

--- packages/presets/dist/PresetClients.d.ts
+++
@@ -1,7 +0,0 @@
-import { Contract as EthersContract } from '@ethersproject/contracts';
-import { MesonClient } from '@meson/sdk';
-export default class PresetClients {
-    private _cache;
-    constructor();
-    getClient(id: any, Contract?: typeof EthersContract): MesonClient;
-}
--- packages/presets/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import MesonPresets from './MesonPresets';
-export { default as mainnets } from './mainnets.json';
-export { default as testnets } from './testnets.json';
-declare const _default: MesonPresets;
-export default _default;
