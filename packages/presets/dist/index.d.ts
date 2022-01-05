840710be70d5dbb7ecf2da808114ef87f9b214b4
Ranmocy <ranmocy@gmail.com>
2022-01-05 18:14:50 -0500

    Remove unneeded files

--- packages/presets/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { MesonClient } from '@meson/sdk';
-declare class Presets {
-    private _cache;
-    constructor();
-    getClient(id: any): MesonClient;
-}
-declare const _default: Presets;
-export default _default;
--- packages/sdk/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { MesonClient, PartialSwapRequest } from './MesonClient';
-export { SwapSigner } from './SwapSigner';
-export { SwapRequest, SwapRequestData } from './SwapRequest';
-export { SwapRequestWithSigner } from './SwapRequestWithSigner';
-export { SignedSwapRequest } from './SignedSwapRequest';
