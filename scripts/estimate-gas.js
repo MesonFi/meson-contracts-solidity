721d86cd8b7881924ceb60f4c3753c550af2076f
zekeluo <425116243@qq.com>
2022-01-17 19:55:58 +0800

    update js

--- scripts/estimate-gas.js
+++ scripts/estimate-gas.js
@@ -6,27 +6,16 @@ const { MesonPoolsTest__factory, MesonSwapTest__factory,MockToken__factory}  = r
 const ethers = require('ethers');
 
 async function main() {
- 
 let provider = ethers.getDefaultProvider('kovan');
 let privateKey = '4719806c5b87c68e046b7b958d4416f66ff752ce60a36d28c0b9c5f29cbc9ab0';
 let wallet = new ethers.Wallet(privateKey, provider);
-
-// 地址来自上面部署的合约
 let tokenAddress = "0xf71D7875682BdD0C47Af795C55DA2B41edd3Ee6B";
-// 使用Provider 连接合约，将只有对合约的可读权限
 let  tokencontract = new ethers.Contract(tokenAddress, MockToken__factory.abi, provider);
 let tokencontractWithSigner = tokencontract.connect(wallet);
- 
-// 地址来自上面部署的合约
 let swapAddress = "0xDa9C9CEdAdC7a6f775150382765ccCA3D4Fa3021";
-// 使用Provider 连接合约，将只有对合约的可读权限
 let  swapcontract = new ethers.Contract(swapAddress, MesonSwapTest__factory.abi, provider);
 let swapcontractWithSigner = swapcontract.connect(wallet);
-
-
-// 地址来自上面部署的合约
 let poolAddress = "0x676D3Bd93ec1f7517D5D12B3DE2AdfF0ABD7B0E6";
-// 使用Provider 连接合约，将只有对合约的可读权限
 let  poolcontract = new ethers.Contract(poolAddress,MesonPoolsTest__factory.abi , provider);
 let poolcontractWithSigner = poolcontract.connect(wallet);
 
@@ -36,8 +25,10 @@ let lpClient = await MesonClient.Create(swapcontractWithSigner)
 
 //##########################---requestSwap---###########################
 const swap = userClient.requestSwap(outChain, getDefaultSwap({ inToken: tokenAddress.address }))
+console.log("#########swap#####",swap)
 const exported = await swap.exportRequest(wallet)
 const signedRequest = new SignedSwapRequest(exported)
+console.log("#########exported#####",exported)
 await tokencontractWithSigner.approve(swapcontractWithSigner.address, swap.amount)
 
 //##########################---postSwap---###########################
@@ -46,8 +37,6 @@ await lpClient.postSwap(signedRequest)
 
 //###########################---lock---###########################
 
-
-//###########################---釋放資金---###########################
 
 
 //###########################---Release---###########################
