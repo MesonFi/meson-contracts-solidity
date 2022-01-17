931162db972e18724c8353dc6de98d7104de88be
Phil Li <qft.gtr@gmail.com>
2022-01-17 13:15:45 -0800

    Update estimate-gas.js

--- scripts/estimate-gas.js
+++ scripts/estimate-gas.js
@@ -1,35 +1,36 @@
-const { MockToken, MesonSwapTest } = require ('@mesonfi/contract-types/types')
-const { getDefaultSwap }  = require ( '../test/shared/meson')
-const { MesonClient, SignedSwapRequest } = require ('@mesonfi/sdk/src')
-const { MesonPoolsTest__factory, MesonSwapTest__factory,MockToken__factory}  = require ( '../packages/contract-types/types/index.ts')
+const { MesonClient, SignedSwapRequest } = require ('@mesonfi/sdk')
+const { Meson, ERC20 }  = require ('@mesonfi/contract-abis')
 
-const ethers = require('ethers');
+const ethers = require('ethers')
+
+const { getDefaultSwap }  = require ( '../test/shared/meson')
 
 async function main() {
-let provider = ethers.getDefaultProvider('kovan');
-let privateKey = '4719806c5b87c68e046b7b958d4416f66ff752ce60a36d28c0b9c5f29cbc9ab0';
+let provider = ethers.getDefaultProvider('ropsten'); // use hardhat network
+// use hardhat default wallet (see test/shared/wallet.ts)
+let privateKey = '0x4719806c5b87c68e046b7b958d4416f66ff752ce60a36d28c0b9c5f29cbc9ab0';
 let wallet = new ethers.Wallet(privateKey, provider);
-let tokenAddress = "0xf71D7875682BdD0C47Af795C55DA2B41edd3Ee6B";
-let  tokencontract = new ethers.Contract(tokenAddress, MockToken__factory.abi, provider);
-let tokencontractWithSigner = tokencontract.connect(wallet);
-let swapAddress = "0xDa9C9CEdAdC7a6f775150382765ccCA3D4Fa3021";
-let  swapcontract = new ethers.Contract(swapAddress, MesonSwapTest__factory.abi, provider);
-let swapcontractWithSigner = swapcontract.connect(wallet);
-let poolAddress = "0x676D3Bd93ec1f7517D5D12B3DE2AdfF0ABD7B0E6";
-let  poolcontract = new ethers.Contract(poolAddress,MesonPoolsTest__factory.abi , provider);
-let poolcontractWithSigner = poolcontract.connect(wallet);
 
-let outChain = await swapcontractWithSigner.getCoinType()
-let userClient = await MesonClient.Create(swapcontractWithSigner)
-let lpClient = await MesonClient.Create(swapcontractWithSigner)
+// deploy contract using scripts
+let tokenAddress = "0x69584d9C068cF71319DfA80CFE1CCF4515816AD9";
+let  tokencontract = new ethers.Contract(tokenAddress, ERC20.abi, wallet);
+let swapAddress = "0xDd72b32f49D1722E22538Dd083e2861823417718";
+let  swapcontract = new ethers.Contract(swapAddress, Meson.abi, wallet);
+// let poolAddress = "0xDd72b32f49D1722E22538Dd083e2861823417718";
+// let  poolcontract = new ethers.Contract(poolAddress, Meson.abi, wallet);
+
+let outChain = await swapcontract.getCoinType()
+let userClient = await MesonClient.Create(swapcontract)
+let lpClient = await MesonClient.Create(swapcontract)
 
 //##########################---requestSwap---###########################
-const swap = userClient.requestSwap(outChain, getDefaultSwap({ inToken: tokenAddress.address }))
-console.log("#########swap#####",swap)
+const swap = userClient.requestSwap(outChain, getDefaultSwap({ inToken: tokenAddress }))
+console.log("#########swap#####", swap)
 const exported = await swap.exportRequest(wallet)
 const signedRequest = new SignedSwapRequest(exported)
-console.log("#########exported#####",exported)
-await tokencontractWithSigner.approve(swapcontractWithSigner.address, swap.amount)
+console.log("#########exported#####", exported)
+const approveTx = await tokencontract.approve(swapcontract.address, swap.amount)
+await approveTx.wait(1)
 
 //##########################---postSwap---###########################
 await lpClient.postSwap(signedRequest)
