cdf3238146a18aede11996e57cf0c500e58f2265
Phil Li <qft.gtr@gmail.com>
2022-01-18 19:56:50 -0800

    revert

--- test/shared/wallet.ts
+++ test/shared/wallet.ts
@@ -9,6 +9,3 @@ export const initiator = new ethers.Wallet(privateKey1, ethers.provider)
 
 const privateKey2 = '0x3618b13685a4faf31003929844655e3c08c864128c45ce356f97de22612ede51'
 export const provider = new ethers.Wallet(privateKey2, ethers.provider)
-
-const privateKey3 ='0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
-export const provider3 = new ethers.Wallet(privateKey3, ethers.provider)
