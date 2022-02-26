5804eb5e8281571733862faa56aba1f48d163b7c
Phil Li <qft.gtr@gmail.com>
2022-02-26 10:23:42 -0800

    Create SwapWithSigner.spec.ts

---
+++ tests_/sdk/SwapWithSigner.spec.ts
@@ -0,0 +1,29 @@
+import {
+  SwapWithSigner,
+} from '@mesonfi/sdk/src'
+
+describe('SwapWithSigner', () => {
+  let swapWithSigner: SwapWithSigner
+  let recipient: string = ''
+
+  beforeEach('create SwapWithSigner', async () => {
+  })
+  
+  describe('#signForRequest', () => {
+    it('signs a swap request for testnet', async () => {
+      swapWithSigner.signForRequest(true)
+    })
+    it('signs a swap request for mainnet', async () => {
+      swapWithSigner.signForRequest(false)
+    })
+  })
+
+  describe('#signForRelease', () => {
+    it('signs a swap release for testnet', async () => {
+      swapWithSigner.signForRelease(recipient, true)
+    })
+    it('signs a swap release for mainnet', async () => {
+      swapWithSigner.signForRelease(recipient, false)
+    })
+  })
+})
