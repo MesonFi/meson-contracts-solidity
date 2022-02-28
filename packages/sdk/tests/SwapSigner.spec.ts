10f1d8053f988584629d80e03b53a066ce5a35e8
zekeluo <425116243@qq.com>
2022-02-28 18:24:31 +0800

    updata  swapSignerAndSwapWithSigner

--- packages/sdk/tests/SwapSigner.spec.ts
+++ packages/sdk/tests/SwapSigner.spec.ts
@@ -1,122 +1,133 @@
 import { expect } from 'chai'
-
+import { solidity, MockProvider } from 'ethereum-waffle';
+import { AddressZero } from '@ethersproject/constants'
 import {
   SwapSigner,
   EthersWalletSwapSigner,
   RemoteSwapSigner,
 } from '../src'
+import { Wallet } from '@ethersproject/wallet'
+
 
 describe('SwapSigner', () => {
   let swapSigner: SwapSigner
-  let encodedSwap: string = ''
-  let recipient: string = ''
+  let ethersWalletSwapSigner: EthersWalletSwapSigner
+  let encodedSwap: string = '0x000000000000000000000064000003c1000000000a00620a4c3e000101000101'
+  let recipient: string = '0x83bcD6A6a860EAac800A45BB1f4c30248e5Dc619'
+  let initiator: Wallet
 
   beforeEach('create SwapSigner', async () => {
+    const wallets = new MockProvider().getWallets()
+    initiator = wallets[1]
+    swapSigner = new SwapSigner
+    ethersWalletSwapSigner = new EthersWalletSwapSigner(initiator)
   })
-  
+
   describe('#getAddress', () => {
     it('returns the signer address"', async () => {
-      swapSigner.getAddress()
+      expect(ethersWalletSwapSigner.getAddress()).to.equal(initiator.address)
     })
   })
 
   describe('#signSwapRequest', () => {
     it('rejects by "Not implemented"', async () => {
-      swapSigner.signSwapRequest(encodedSwap)
+
+      try {
+        await swapSigner.signSwapRequest(encodedSwap)
+      } catch (error) {
+        expect(error).to.match(/Not implemented/)
+      }
     })
   })
 
   describe('#signSwapRelease', () => {
     it('rejects by "Not implemented"', async () => {
-      swapSigner.signSwapRelease(encodedSwap, recipient)
+      try {
+        await swapSigner.signSwapRelease(encodedSwap, recipient)
+      } catch (error) {
+        expect(error).to.match(/Not implemented/)
+      }
     })
   })
 
   describe('#SwapSigner.hashRequest', () => {
     it('hashes a swap request for testnet', async () => {
-      SwapSigner.hashRequest(encodedSwap, true)
+      expect(SwapSigner.hashRequest(encodedSwap, false)).to.not.null
     })
 
     it('hashes a swap request for mainnet', async () => {
-      SwapSigner.hashRequest(encodedSwap, false)
+      expect(SwapSigner.hashRequest(encodedSwap, false)).to.not.null
     })
+
+
+    describe('#SwapSigner.hashRelease', () => {
+      it('hashes a swap release for testnet', async () => {
+        expect(SwapSigner.hashRelease(encodedSwap, recipient, true)).to.not.null
+      })
+
+      it('hashes a swap release for mainnet', async () => {
+        expect(SwapSigner.hashRelease(encodedSwap, recipient, false)).to.not.null
+      })
+    })
+
+    describe('EthersWalletSwapSigner', () => {
+      describe('#getAddress', () => {
+        it('returns the signer address', async () => {
+          expect(ethersWalletSwapSigner.getAddress()).to.equal(initiator.address)
+        })
+      })
+
+      describe('#signSwapRequest', () => {
+        it('signs a swap request for testnet', async () => {
+          expect(ethersWalletSwapSigner.signSwapRequest(encodedSwap, true)).to.not.null
+        })
+        it('signs a swap request for mainnet', async () => {
+          expect(ethersWalletSwapSigner.signSwapRequest(encodedSwap)).to.not.null
+        })
+      })
+
+      describe('#signSwapRelease', () => {
+        it('signs a swap release for testnet', async () => {
+          expect(ethersWalletSwapSigner.signSwapRelease(encodedSwap, recipient, true)).to.not.null
+
+        })
+        it('signs a swap release for mainnet', async () => {
+          expect(ethersWalletSwapSigner.signSwapRelease(encodedSwap, recipient, false)).to.not.null
+        })
+      })
+    })
+
+
+    //   describe('RemoteSwapSigner', () => {
+    //     let swapSigner: RemoteSwapSigner
+    //     let encodedSwap: string = ''
+    //     let recipient: string = ''
+
+    //     beforeEach('create RemoteSwapSigner', async () => {
+    //       swapSigner = new RemoteSwapSigner()
+    //     })
+
+    //     describe('#getAddress', () => {
+    //       it('rejects by "Not implemented"', async () => {
+    //         swapSigner.getAddress()
+    //       })
+    //     })
+
+    //     describe('#signSwapRequest', () => {
+    //       it('signs a swap request for testnet', async () => {
+    //         swapSigner.signSwapRequest(encodedSwap, true)
+    //       })
+    //       it('signs a swap request for mainnet', async () => {
+    //         swapSigner.signSwapRequest(encodedSwap, false)
+    //       })
+    //     })
+
+    //     describe('#signSwapRelease', () => {
+    //       it('signs a swap release for testnet', async () => {
+    //         swapSigner.signSwapRelease(encodedSwap, recipient, true)
+    //       })
+    //       it('signs a swap release for mainnet', async () => {
+    //         swapSigner.signSwapRelease(encodedSwap, recipient, false)
   })
 
-  describe('#SwapSigner.hashRelease', () => {
-    it('hashes a swap release for testnet', async () => {
-      SwapSigner.hashRelease(encodedSwap, recipient, true)
-    })
-
-    it('hashes a swap release for mainnet', async () => {
-      SwapSigner.hashRelease(encodedSwap, recipient, false)
-    })
-  })
-})
-
-
-describe('EthersWalletSwapSigner', () => {
-  let swapSigner: EthersWalletSwapSigner
-  let encodedSwap: string = ''
-  let recipient: string = ''
-
-  beforeEach('create EthersWalletSwapSigner', async () => {
-  })
-  
-  describe('#getAddress', () => {
-    it('returns the signer address', async () => {
-      swapSigner.getAddress()
-    })
-  })
-
-  describe('#signSwapRequest', () => {
-    it('signs a swap request for testnet', async () => {
-      swapSigner.signSwapRequest(encodedSwap, true)
-    })
-    it('signs a swap request for mainnet', async () => {
-      swapSigner.signSwapRequest(encodedSwap, false)
-    })
-  })
-
-  describe('#signSwapRelease', () => {
-    it('signs a swap release for testnet', async () => {
-      swapSigner.signSwapRelease(encodedSwap, recipient, true)
-    })
-    it('signs a swap release for mainnet', async () => {
-      swapSigner.signSwapRelease(encodedSwap, recipient, false)
-    })
-  })
-})
-
-
-describe('RemoteSwapSigner', () => {
-  let swapSigner: RemoteSwapSigner
-  let encodedSwap: string = ''
-  let recipient: string = ''
-
-  beforeEach('create RemoteSwapSigner', async () => {
-  })
-  
-  describe('#getAddress', () => {
-    it('rejects by "Not implemented"', async () => {
-      swapSigner.getAddress()
-    })
-  })
-
-  describe('#signSwapRequest', () => {
-    it('signs a swap request for testnet', async () => {
-      swapSigner.signSwapRequest(encodedSwap, true)
-    })
-    it('signs a swap request for mainnet', async () => {
-      swapSigner.signSwapRequest(encodedSwap, false)
-    })
-  })
-
-  describe('#signSwapRelease', () => {
-    it('signs a swap release for testnet', async () => {
-      swapSigner.signSwapRelease(encodedSwap, recipient, true)
-    })
-    it('signs a swap release for mainnet', async () => {
-      swapSigner.signSwapRelease(encodedSwap, recipient, false)
-    })
-  })
 })
--- packages/sdk/tests/SwapWithSigner.spec.ts
+++ packages/sdk/tests/SwapWithSigner.spec.ts
@@ -1,31 +1,42 @@
 import { expect } from 'chai'
-
+import { Wallet } from '@ethersproject/wallet'
+import { getSwap } from './shared'
+import { solidity, MockProvider } from 'ethereum-waffle';
 import {
   SwapWithSigner,
+  EthersWalletSwapSigner
 } from '../src'
+ 
 
 describe('SwapWithSigner', () => {
   let swapWithSigner: SwapWithSigner
-  let recipient: string = ''
-
+  let initiator: Wallet
+  let recipient: Wallet
   beforeEach('create SwapWithSigner', async () => {
+    const wallets = new MockProvider().getWallets()
+    initiator = wallets[1]
+    recipient = wallets[3]
+    const swapSigner = new EthersWalletSwapSigner(initiator)
+    swapWithSigner = new SwapWithSigner(getSwap(), swapSigner)
   })
   
   describe('#signForRequest', () => {
     it('signs a swap request for testnet', async () => {
-      swapWithSigner.signForRequest(true)
+      expect( await (await swapWithSigner.signForRequest(true)).initiator).to.equal(initiator.address)  
     })
     it('signs a swap request for mainnet', async () => {
-      swapWithSigner.signForRequest(false)
+      expect( await (await swapWithSigner.signForRequest(false)).initiator).to.equal(initiator.address)  
     })
   })
 
   describe('#signForRelease', () => {
     it('signs a swap release for testnet', async () => {
-      swapWithSigner.signForRelease(recipient, true)
+      expect( (await swapWithSigner.signForRelease(recipient.address, true)).initiator).to.equal(initiator.address)
+      expect( (await swapWithSigner.signForRelease(recipient.address, true)).recipient).to.equal(recipient.address)
     })
     it('signs a swap release for mainnet', async () => {
-      swapWithSigner.signForRelease(recipient, false)
+      expect( (await swapWithSigner.signForRelease(recipient.address, false)).initiator).to.equal(initiator.address)
+      expect( (await swapWithSigner.signForRelease(recipient.address, false)).recipient).to.equal(recipient.address)
     })
   })
 })
