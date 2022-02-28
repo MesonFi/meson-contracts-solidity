051e6282b5080bc7dc7a48f3e02d9f6631ccd028
Phil Li <qft.gtr@gmail.com>
2022-02-27 17:47:07 -0800

    Update MesonSwap.spec.ts

--- test/MesonSwap.spec.ts
+++ test/MesonSwap.spec.ts
@@ -1,292 +1,185 @@
-import { ethers, upgrades, waffle } from 'hardhat'
+import { ethers, waffle } from 'hardhat'
 import {
   MesonClient,
   EthersWalletSwapSigner,
+  SwapWithSigner,
   SignedSwapRequest,
-  SignedSwapRelease
+  SignedSwapRelease,
 } from '@mesonfi/sdk'
+import { MockToken, MesonSwapTest } from '@mesonfi/contract-typs'
 import { keccak256 } from '@ethersproject/keccak256'
 import { pack } from '@ethersproject/solidity'
 
 import { expect } from './shared/expect'
 import { initiator, provider } from './shared/wallet'
-import { getDefaultSwap } from './shared/meson'
+import { fixtures, TOKEN_BALANCE } from './shared/fixtures'
+import { getPartialSwap } from './shared/meson'
+
+const testnetMode = true
+const outChain = '0x1234'
 
 describe('MesonSwap', () => {
-  let totalSupply = 1000000000000
-  let mesonContract;
-  let tokenContract;
-  let tokenUsdcContract;
-  let mesonFactory;
-  let userClient: MesonClient
-  let signedRequest
-  let outChain
-  let swap
-  let swapData
-  const testnetMode = true
-  describe('MesonSwap', () => {
-    beforeEach('deploy MesonSwapTest', async () => {
-      const MockToken = await ethers.getContractFactory('MockToken')
-      tokenContract = await MockToken.deploy('Mock Token Usdt', 'MUsdt', totalSupply)
-      await tokenContract.deployed()
-      tokenUsdcContract = await MockToken.deploy('Mock Token Usdc', 'MUsdc', totalSupply)
-      await tokenUsdcContract.deployed()
-      mesonFactory = await ethers.getContractFactory('UpgradableMeson')
-      mesonContract = await upgrades.deployProxy(mesonFactory, [[tokenContract.address, tokenUsdcContract.address]], { kind: 'uups' })
-      await mesonContract.deployed()
+  let token: MockToken
+  let unsupportedToken: MockToken
 
-      let approve = await tokenContract.approve(mesonContract.address, 10000000000)
-      await approve
-      userClient = await MesonClient.Create(mesonContract, new EthersWalletSwapSigner(provider))
-      outChain = await mesonContract.getShortCoinType()
-      swap = userClient.requestSwap(getDefaultSwap(), outChain)
+  let mesonInstance: MesonSwapTest
+  let mesonClientForInitiator: MesonClient
+  let mesonClientForProvider: MesonClient
+
+  let swap: SwapWithSigner
+  let signedRequest: SignedSwapRequest
+
+  beforeEach('deploy MesonSwapTest', async () => {
+    const result = await waffle.loadFixture(() => fixtures([
+      initiator.address, provider.address
+    ]))
+    token = result.token1.connect(initiator)
+    unsupportedToken = result.token2.connect(initiator)
+    mesonInstance = result.swap // default account is signer
+
+    const swapSigner = new EthersWalletSwapSigner(initiator)
+    mesonClientForInitiator = await MesonClient.Create(mesonInstance.connect(initiator), swapSigner) // user is default account
+    mesonClientForProvider = await MesonClient.Create(mesonInstance.connect(provider))
+    
+    await mesonClientForProvider.mesonInstance.register(1)
+
+    swap = mesonClientForInitiator.requestSwap(getPartialSwap(), outChain)
+    const signedRequestData = await swap.signForRequest(true)
+    signedRequest = new SignedSwapRequest(signedRequestData)
+  })
+
+
+  describe('#postSwap', () => {
+    it('rejects incorrect inChain', async () => {
+      const swap2 = new SwapWithSigner({ ...swap.toObject(), inChain: '0x0001' }, swap.swapSigner)
+      const signedRequestData = await swap2.signForRequest(true)
+      const signedRequest = new SignedSwapRequest(signedRequestData)
+
+      await expect(mesonClientForProvider.postSwap(signedRequest)).to.be.revertedWith('Swap not for this chain')
+    })
+    it('rejects unsupported token', async () => {
+      const swap = mesonClientForInitiator.requestSwap(getPartialSwap({ inToken: 2 }), outChain)
+      const signedRequestData = await swap.signForRequest(true)
+      const signedRequest = new SignedSwapRequest(signedRequestData)
+
+      await expect(mesonClientForProvider.postSwap(signedRequest))
+        .to.be.revertedWith('function call to a non-contract account')
+    })
+    it('rejects if expireTs is too early', async () => {
+      const swap = mesonClientForInitiator.requestSwap(getPartialSwap(), outChain, 3600 - 600)
+      const signedRequestData = await swap.signForRequest(true)
+      const signedRequest = new SignedSwapRequest(signedRequestData)
+
+      await expect(mesonClientForProvider.postSwap(signedRequest)).to.be.revertedWith('Expire ts too early')
+    })
+    it('rejects if expireTs is too late ', async () => {
+      const swap = mesonClientForInitiator.requestSwap(getPartialSwap(), outChain, 7200 + 600)
+      const signedRequestData = await swap.signForRequest(true)
+      const signedRequest = new SignedSwapRequest(signedRequestData)
+
+      await expect(mesonClientForProvider.postSwap(signedRequest)).to.be.revertedWith('Expire ts too late')
+    })
+    it('rejects if amount > 100k', async () => {
+      const swap = mesonClientForInitiator.requestSwap(getPartialSwap({ amount: '100000000001' }), outChain)
+      const signedRequestData = await swap.signForRequest(true)
+      const signedRequest = new SignedSwapRequest(signedRequestData)
+
+      await expect(mesonClientForProvider.postSwap(signedRequest))
+        .to.be.revertedWith('For security reason, amount cannot be greater than 100k')
+    })
+    it('posts a valid swap', async () => {
+      await token.connect(initiator).approve(mesonInstance.address, 101)
+      await mesonClientForProvider.postSwap(signedRequest)
+
+      const posted = await mesonInstance.getPostedSwap(swap.encoded)
+      expect(posted.initiator).to.equal(initiator.address)
+      expect(posted.provider).to.equal(provider.address)
+      expect(await token.balanceOf(initiator.address)).to.equal(TOKEN_BALANCE.sub(101))
+    })
+    it('rejects if swap already exists', async () => {
+      await token.connect(initiator).approve(mesonInstance.address, 101)
+      await mesonClientForProvider.postSwap(signedRequest)
+
+      await expect(mesonClientForProvider.postSwap(signedRequest)).to.be.revertedWith('Swap already exists')
+    })
+  })
+
+  describe('#executeSwap', () => {
+    it('accepts  bondSwap', async () => {
+    })
+    it('rejects swap does not exist ', async () => {
+    })
+    it('rejects Swap bonded to another provider ', async () => {
+    })
+    it('rejects  Can only bound to signer', async () => {
+    })
+    it('rejects swap does not exist', async () => {
+    })
+    it('can execute a swap', async () => {
+      const swapData = getDefaultSwap({ fee: '0' })
+      const swap = userClient.requestSwap(swapData, outChain)
       const request = await swap.signForRequest(testnetMode)
-      signedRequest = new SignedSwapRequest(request)
+      
+      const signedRequest = new SignedSwapRequest(request)
       signedRequest.checkSignature(testnetMode)
-      swapData = getDefaultSwap()
+      await token.approve(mesonInstance.address, swap.amount)
+      await lpClient.postSwap(signedRequest)
+
+      const release = await swap.signForRelease(swapData.recipient, testnetMode)
+      const signedRelease = new SignedSwapRelease(release)
+      signedRelease.checkSignature(testnetMode)
+      await lpClient.executeSwap(signedRelease, false)
+
+      const posted = await mesonInstance.getPostedSwap(swap.encoded)
+      expect(posted.initiator).to.equal(ethers.constants.AddressZero)
+      expect(posted.provider).to.equal(ethers.constants.AddressZero)
+      expect(await token.balanceOf(initiator.address)).to.equal(TOKEN_BALANCE.sub(swap.amount))
+      expect(await token.balanceOf(provider.address)).to.equal(TOKEN_BALANCE.add(swap.amount))
     })
+  })
 
-    describe('#postSwap', () => {
-      it('rejects Swap already exists', async () => {
-
-
-        let balanceIndex = 0x010000000001
-        let amount = 1000000000000
-        let providerIndex: string = '1'
-        let approve = await tokenContract.approve(mesonContract.address, amount)
-        await approve
-        await mesonContract.depositAndRegister('10000000', balanceIndex)
-        swap = userClient.requestSwap(getDefaultSwap({ amount: '10' }), outChain)
-        const request = await swap.signForRequest(testnetMode)
-        signedRequest = new SignedSwapRequest(request)
-        await mesonContract.postSwap(
-          signedRequest.encoded,
-          signedRequest.signature[0],
-          signedRequest.signature[1],
-          signedRequest.signature[2],
-          pack(['address', 'uint40'], [
-            signedRequest.initiator,
-            providerIndex
-          ])
-        )
-        try {
-          await mesonContract.postSwap(
-            signedRequest.encoded,
-            signedRequest.signature[0],
-            signedRequest.signature[1],
-            signedRequest.signature[2],
-            pack(['address', 'uint40'], [
-              signedRequest.initiator,
-              providerIndex
-            ])
-          )
-        } catch (error) {
-          expect(error).to.match(/Swap already exists/)
-        }
-      })
-      it('rejects expire ts too early', async () => {
-        //How to Change the expire time
-      })
-      it('rejects expire ts too late ', async () => {
-        //expect check delta = MAX_BOND_TIME_PERIOD
-      })
-      it('rejects  expire ts too late ', async () => {
-        //expect check delta < MAX_BOND_TIME_PERIOD
-      })
-      it('expect For security reason, amount cannot be greater than 100k', async () => {
-        let balanceIndex = 0x010000000001
-        let amount = 1000000000000
-        let providerIndex: string = '1'
-        let approve = await tokenContract.approve(mesonContract.address, amount)
-        await approve
-        console.log(await tokenContract.balanceOf(initiator.address))
-        await mesonContract.depositAndRegister('100000000000', balanceIndex)
-        swap = userClient.requestSwap(getDefaultSwap({ amount: '1000000000000' }), outChain)
-        const request = await swap.signForRequest(testnetMode)
-        signedRequest = new SignedSwapRequest(request)
-        try {
-          await mesonContract.postSwap(
-            signedRequest.encoded,
-            signedRequest.signature[0],
-            signedRequest.signature[1],
-            signedRequest.signature[2],
-            pack(['address', 'uint40'], [
-              signedRequest.initiator,
-              providerIndex
-            ])
-          )
-        } catch (error) {
-          expect(error).to.match(/For security reason, amount cannot be greater than 100k/)
-        }
-      })
-      it('accepts  the postSwap if all parameters are correct', async () => {
-        let providerIndex: string = '1'
-        await mesonContract.postSwap(
-          signedRequest.encoded,
-          signedRequest.signature[0],
-          signedRequest.signature[1],
-          signedRequest.signature[2],
-          pack(['address', 'uint40'], [
-            signedRequest.initiator,
-            providerIndex
-          ])
-        )
-        const getPostSwap = await mesonContract.getPostedSwap(signedRequest.encoded)
-        expect(getPostSwap.initiator.toLowerCase()).to.equal(signedRequest.initiator)
-        expect(getPostSwap.executed).to.be.false
-      })
+  describe('#cancelSwap', () => {
+    it('accepts cancelSwap', async () => {
+      //How to cancelswap It takes 30 minutes to unlock and then cancel
     })
-
-      describe('#bondSwap', () => {
-        it('accepts  bondSwap', async () => {
-          //  let providerIndex: string = '1'
-          //   await mesonContract.postSwap(
-          //     signedRequest.encoded,
-          //     signedRequest.signature[0],
-          //     signedRequest.signature[1],
-          //     signedRequest.signature[2],
-          //     pack(['address', 'uint40'], [
-          //       signedRequest.initiator,
-          //       providerIndex
-          //     ])
-          //   )
-          //   await mesonContract.bondSwap(signedRequest.encoded, providerIndex)
-        })
-        it('rejects swap does not exist ', async () => {
-          try {
-            let providerIndex: string = '1'
-            await mesonContract.bondSwap(signedRequest.encoded + '1', providerIndex)
-          } catch (error) {
-            expect(error).to.match(/Swap does not exist/)
-          }
-        })
-        it('rejects Swap bonded to another provider ', async () => {
-          let providerIndex: string = '1'
-          await mesonContract.postSwap(
-            signedRequest.encoded,
-            signedRequest.signature[0],
-            signedRequest.signature[1],
-            signedRequest.signature[2],
-            pack(['address', 'uint40'], [
-              signedRequest.initiator,
-              providerIndex
-            ])
-          )
-          try {
-            await mesonContract.bondSwap(signedRequest.encoded, providerIndex)
-          } catch (error) {
-            expect(error).to.match(/Swap bonded to another provider/)
-          }
-        })
-        it('rejects  Can only bound to signer', async () => {
-          //   let contract = new ethers.Contract(mesonContract.address, UpgradableMeson__factory.abi, provider);
-          //   let providerIndex: string = '3'
-          //   await mesonContract.postSwap(
-          //     signedRequest.encoded,
-          //     signedRequest.signature[0],
-          //     signedRequest.signature[1],
-          //     signedRequest.signature[2],
-          //     pack(['address', 'uint40'], [
-          //       signedRequest.initiator,
-          //       providerIndex
-          //     ])
-          //   )
-          //   try {
-          //     await contract.bondSwap(signedRequest.encoded, providerIndex, over)
-          //   } catch (error) {
-          //     console.log(error)
-          //     expect(error).to.match(/Can only bound to signer/)
-          //   }
-        })
-      })
-      describe('#executeSwap', () => {
-        it('accepts executeSwap', async () => {
-          // let providerIndex: string = '1'
-          // const swapData = getDefaultSwap({ fee: '0' })
-          // const swap = userClient.requestSwap(swapData, outChain)
-          // const request = await swap.signForRequest(testnetMode)
-          // const signedRequest = new SignedSwapRequest(request)
-          // signedRequest.checkSignature(testnetMode)
-          // await mesonContract.postSwap(
-          //   signedRequest.encoded,
-          //   signedRequest.signature[0],
-          //   signedRequest.signature[1],
-          //   signedRequest.signature[2],
-          //   pack(['address', 'uint40'], [
-          //     signedRequest.initiator,
-          //     providerIndex
-          //   ])
-          // )
-          // const release = await swap.signForRelease(swapData.recipient, testnetMode)
-          // const signedRelease = new SignedSwapRelease(release)
-          // signedRelease.checkSignature(testnetMode)
-          // await mesonContract.executeSwap(
-          //   signedRelease.encoded,
-          //   keccak256(signedRelease.recipient),
-          //   ...signedRelease.signature,
-          //   false
-          // )
-        })
-        it('rejects swap does not exist', async () => {
-          const release = await swap.signForRelease(swapData.recipient, testnetMode)
-          const signedRelease = new SignedSwapRelease(release)
-          signedRelease.checkSignature(testnetMode)
-          try {
-            await mesonContract.executeSwap(
-              signedRelease.encoded + '1',
-              keccak256(signedRelease.recipient),
-              ...signedRelease.signature,
-              false
-            )
-          } catch (error) {
-            expect(error).to.match(/Swap does not exist/)
-          }
-        })
-      })
-
-      describe('#cancelSwap', () => {
-        it('accepts cancelSwap', async () => {
-          //How to cancelswap It takes 30 minutes to unlock and then cancel
-        })
-        it('rejects Swap does not exist ', async () => {
-          await mesonContract.postSwap(
-            signedRequest.encoded,
-            signedRequest.signature[0],
-            signedRequest.signature[1],
-            signedRequest.signature[2],
-            pack(['address', 'uint40'], [
-              signedRequest.initiator,
-              '1'
-            ])
-          )
-          try {
-            await mesonContract.cancelSwap(signedRequest.encoded + '1')
-          } catch (error) {
-            expect(error).to.match(/Swap does not exist/)
-          }
-        })
-        it('rejects  Swap is still locked ', async () => {
-          const swapData = getDefaultSwap({ fee: '0' })
-          const swap = userClient.requestSwap(swapData, outChain)
-          const request = await swap.signForRequest(testnetMode)
-          const signedRequest = new SignedSwapRequest(request)
-          signedRequest.checkSignature(testnetMode)
-          await mesonContract.postSwap(
-            signedRequest.encoded,
-            signedRequest.signature[0],
-            signedRequest.signature[1],
-            signedRequest.signature[2],
-            pack(['address', 'uint40'], [
-              signedRequest.initiator,
-              '1'
-            ])
-          )
-          try {
-            await mesonContract.cancelSwap(signedRequest.encoded)
-          } catch (error) {
-            expect(error).to.match(/Swap is still locked/)
-          }
-        })
-      })
+    it('rejects Swap does not exist ', async () => {
+      await mesonContract.postSwap(
+        signedRequest.encoded,
+        signedRequest.signature[0],
+        signedRequest.signature[1],
+        signedRequest.signature[2],
+        pack(['address', 'uint40'], [
+          signedRequest.initiator,
+          '1'
+        ])
+      )
+      try {
+        await mesonContract.cancelSwap(signedRequest.encoded + '1')
+      } catch (error) {
+        expect(error).to.match(/Swap does not exist/)
+      }
+    })
+    it('rejects  Swap is still locked ', async () => {
+      const swapData = getDefaultSwap({ fee: '0' })
+      const swap = userClient.requestSwap(swapData, outChain)
+      const request = await swap.signForRequest(testnetMode)
+      const signedRequest = new SignedSwapRequest(request)
+      signedRequest.checkSignature(testnetMode)
+      await mesonContract.postSwap(
+        signedRequest.encoded,
+        signedRequest.signature[0],
+        signedRequest.signature[1],
+        signedRequest.signature[2],
+        pack(['address', 'uint40'], [
+          signedRequest.initiator,
+          '1'
+        ])
+      )
+      try {
+        await mesonContract.cancelSwap(signedRequest.encoded)
+      } catch (error) {
+        expect(error).to.match(/Swap is still locked/)
+      }
+    })
   })
 })
