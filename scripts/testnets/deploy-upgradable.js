a9ab15c7053f69bdbed2d276b6d8c120fb788431
Phil <qft.gtr@gmail.com>
2022-03-03 16:23:28 -0800

    update MockToken and deploy

--- scripts/testnets/deploy-upgradable.js
+++ scripts/testnets/deploy-upgradable.js
@@ -18,40 +18,24 @@ async function main() {
 
   const wallet = new ethers.Wallet(PRIVATE_KEY, ethers.provider)
   const MockToken = await ethers.getContractFactory('MockToken', wallet)
-  const nonce = await ethers.provider.getTransactionCount(wallet.address)
-  const tokens = []
-  const totalSupply = ethers.utils.parseUnits('1000000', 6)
-  
-  const usdt = {
-    name: `${testnet.alias} USDT`,
-    symbol: `${NETWORK_ID[0]}USDT`,
-    decimals: 6
-  }
-  console.log(`Deploying ${usdt.name}...`)
-  const mockUSDT = await MockToken.deploy(usdt.name, usdt.symbol, totalSupply, { nonce })
-  await mockUSDT.deployed()
-  usdt.addr = mockUSDT.address
-  tokens.push(usdt)
-  console.log(`${usdt.name} deployed to:`, mockUSDT.address)
 
-  const usdc = {
-    name: `${testnet.alias} USDC`,
-    symbol: `${NETWORK_ID[0]}USDC`,
-    decimals: 6
+  const tokens = []
+  for await(const token of testnet.tokens) {
+    const totalSupply = ethers.utils.parseUnits('1000000', token.decimals)
+    console.log(`Deploying ${token.name}...`)
+    const tokenContract = await MockToken.deploy(token.name, token.symbol, totalSupply, token.decimals)
+    await tokenContract.deployed()
+    token.addr = tokenContract.address
+    tokens.push({ ...token, contract: tokenContract })
+    console.log(`${token.name} deployed to:`, token.addr)
   }
-  console.log(`Deploying ${usdc.name}...`)
-  const mockUSDC = await MockToken.deploy(usdc.name, usdc.symbol, totalSupply, { nonce: nonce + 1 })
-  await mockUSDC.deployed()
-  usdc.addr = mockUSDC.address
-  tokens.push(usdc)
-  console.log(`${usdc.name} deployed to:`, mockUSDC.address)
 
   const UpgradableMeson = await ethers.getContractFactory('UpgradableMeson', wallet)
   console.log('Deploying UpgradableMeson...')
   const meson = await upgrades.deployProxy(
     UpgradableMeson,
-    [[mockUSDT.address, mockUSDC.address]],
-    { kind: 'uups', nonce: nonce + 2 }
+    [tokens.map(t => t.addr)],
+    { kind: 'uups' }
   )
   await meson.deployed()
   console.log('UpgradableMeson deployed to:', meson.address)
@@ -62,22 +46,24 @@ async function main() {
     throw new Error('Coin type does not match')
   }
 
+  let needRegister = true
   if (DEPOSIT_ON_DEPLOY) {
-    const depositAmount = ethers.utils.parseUnits(DEPOSIT_ON_DEPLOY, 6)
-
-    console.log(`Approving and depositing ${depositAmount} ${usdt.symbol} to Meson...`)
-    await (await mockUSDT.approve(meson.address, depositAmount)).wait(1)
-    await (await mesonClient.depositAndRegister(mesonClient.token(1), depositAmount, 1)).wait(1)
-    console.log(`${depositAmount} ${usdt.symbol} deposited`)
-
-    console.log(`Approving and depositing ${depositAmount} ${usdc.symbol} to Meson...`)
-    await (await mockUSDC.approve(meson.address, depositAmount)).wait(1)
-    await (await mesonClient.deposit(mesonClient.token(2), depositAmount)).wait(1)
-    console.log(`${depositAmount} ${usdc.symbol} deposited`)
+    for await(const token of tokens) {
+      const value = ethers.utils.parseUnits(DEPOSIT_ON_DEPLOY, token.decimals)
+      const valueForMeson = ethers.utils.parseUnits(DEPOSIT_ON_DEPLOY, 6)
+      console.log(`Approving and depositing ${DEPOSIT_ON_DEPLOY} ${token.symbol} to Meson...`)
+      await (await token.contract.approve(meson.address, value)).wait(1)
+      if (needRegister) {
+        await (await mesonClient.depositAndRegister(token.addr, valueForMeson, 1)).wait(1)
+        needRegister = false
+      } else {
+        await (await mesonClient.deposit(token.addr, valueForMeson)).wait(1)
+      }
+      console.log(`${DEPOSIT_ON_DEPLOY} ${token.symbol} deposited`)
+    }
   }
 
   testnet.mesonAddress = meson.address
-  testnet.tokens = tokens
   testnets.splice(index, 1, testnet)
   const testnetsPath = path.join(__dirname, '../../packages/presets/src/testnets.json')
   fs.writeFileSync(testnetsPath, JSON.stringify(testnets, null, 2))
