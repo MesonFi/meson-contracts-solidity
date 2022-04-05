0bddfc3480ac558b2926fe3da697e5252404ae09
Phil Li <qft.gtr@gmail.com>
2022-04-05 10:42:46 -0700

    Update deposit.js

--- scripts/tron/deposit.js
+++ scripts/tron/deposit.js
@@ -11,7 +11,8 @@ const usdt = 'TFa74kDVGad7Lhwe2cwqgUQQY6D65odv2t'
 const usdc = 'TWpuhvz3tivwoQcm16kzaChAZHv2QRGcm5'
 const meson = 'TYtGRRt9ZcAtYEEVG4B5vGuxAWsrQ2WTFo'
 
-const amount = ethers.utils.parseUnits('10000', 6)
+const amount = ethers.utils.parseUnits('50000', 6)
+const lpIndex = 3
 
 async function deposit() {
   const usdtContract = await tronWeb.contract(ERC20.abi, usdt)
@@ -20,11 +21,11 @@ async function deposit() {
 
   await usdtContract.approve(meson, amount).send()
   await usdcContract.approve(meson, amount).send()
-
   await new Promise(resolve => setInterval(resolve, 1000))
 
-  await mesonContract.depositAndRegister(amount, 2**40 + 1).send()
-  await mesonContract.deposit(amount, 2 * 2**40 + 1).send()
+  await mesonContract.depositAndRegister(amount, 2**40 + lpIndex).send()
+  await new Promise(resolve => setInterval(resolve, 1000))
+  await mesonContract.deposit(amount, 2 * 2**40 + lpIndex).send()
 
   // await mesonContract.withdraw(amount, 1).send()
   // await mesonContract.withdraw(amount, 2).send()
