a500aa941707832a46cc7d46fbf4b76f33d6cf1d
Phil Li <qft.gtr@gmail.com>
2022-04-06 23:05:59 -0700

    update testnet scripts

--- scripts/testnets/upgrade.js
+++ scripts/testnets/upgrade.js
@@ -2,10 +2,11 @@ const { ethers, upgrades } = require('hardhat')
 require('dotenv').config()
 
 const { NETWORK_ID, PRIVATE_KEY } = process.env
+const networkId = NETWORK_ID === 'eth' ? 'ropsten' : NETWORK_ID
 
 async function main() {
   const testnets = require('@mesonfi/presets/src/testnets.json')
-  const network = testnets.find(item => item.id.startsWith(NETWORK_ID))
+  const network = testnets.find(item => item.id.startsWith(networkId))
   hre.changeNetwork(network.id)
 
   const wallet = new ethers.Wallet(PRIVATE_KEY, ethers.provider)
