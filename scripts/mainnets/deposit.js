2eb17998ceb2e26c1265c5e9767405aa7a9448c5
Phil Li <qft.gtr@gmail.com>
2022-04-21 17:32:46 -0700

    Update deposit.js

--- scripts/mainnets/deposit.js
+++ scripts/mainnets/deposit.js
@@ -6,22 +6,30 @@ require('dotenv').config()
 
 const { HARDHAT_NETWORK, LP_PRIVATE_KEY } = process.env
 
-const amount = '10000'
+const amount = '100000'
 const tokenIndex = 1
 
+const mainnets = require('@mesonfi/presets/src/mainnets.json')
+const network = mainnets.find(item => item.id === HARDHAT_NETWORK)
+ethers.provider = new CustomGasFeeProviderWrapper(ethers.provider)
+
+const wallet = new ethers.Wallet(LP_PRIVATE_KEY, ethers.provider)
+
+main()
+
 async function main() {
-  const mainnets = require('@mesonfi/presets/src/mainnets.json')
-  const network = mainnets.find(item => item.id === HARDHAT_NETWORK)
-  ethers.provider = new CustomGasFeeProviderWrapper(ethers.provider)
-
-  const wallet = new ethers.Wallet(LP_PRIVATE_KEY, ethers.provider)
-
   const tokenAddr = tokenIndex === 255 ? network.mesonToken : network.tokens.find(t => t.tokenIndex === tokenIndex).addr
   const token = new ethers.Contract(tokenAddr, ERC20.abi, wallet)
   const meson = new ethers.Contract(network.mesonAddress, Meson.abi, wallet)
 
   console.log(`lp address: ${wallet.address}`)
 
+  const tx = await deposit(meson, token)
+  // const tx = await withdraw(meson, token)
+  console.log(tx)
+}
+
+async function deposit (meson, token) {
   console.log(`approving for ${amount}...`)
   const decimals = await token.decimals()
   await (await token.approve(network.mesonAddress, ethers.utils.parseUnits(amount, decimals))).wait(1)
@@ -31,15 +39,16 @@ async function main() {
   const needRegister = providerIndex == 0
   const balanceIndex = tokenIndex * 2**40 + (needRegister ? 1 : providerIndex)
 
-  let tx
   if (needRegister) {
-    tx = await meson.depositAndRegister(ethers.utils.parseUnits(amount, 6), balanceIndex)
+    return await meson.depositAndRegister(ethers.utils.parseUnits(amount, 6), balanceIndex)
   } else {
-    tx = await meson.deposit(ethers.utils.parseUnits(amount, 6), balanceIndex)
+    return await meson.deposit(ethers.utils.parseUnits(amount, 6), balanceIndex)
   }
-  // await (await meson.withdraw(ethers.utils.parseUnits(amount, 6), tokenIndex)).wait()
-  // tx = await token.transfer('', ethers.utils.parseUnits(amount, decimals))
-  console.log(tx)
 }
 
-main()
+async function withdraw (meson, token) {
+  const decimals = await token.decimals()
+  console.log(`withdrawing for ${amount}...`)
+  await (await meson.withdraw(ethers.utils.parseUnits(amount, 6), tokenIndex)).wait()
+  return await token.transfer('', ethers.utils.parseUnits(amount, decimals))
+}
