fd39920cfde85bc25a41d46c8ae6441058a7ce77
Phil Li <qft.gtr@gmail.com>
2022-08-06 12:03:50 -0500

    Update MesonPools.sol

--- contracts/Pools/MesonPools.sol
+++ contracts/Pools/MesonPools.sol
@@ -71,6 +71,26 @@ contract MesonPools is IMesonPoolsEvents, MesonStates {
     _safeTransfer(_tokenList[tokenIndex], _msgSender(), amount, tokenIndex == 255);
   }
 
+  /// TODO: @wyf describe this method
+  function addAuthorizedAddr(address addr) external {
+    require(poolOfAuthorizedAddr[addr] == 0, "Addr is authorized for another pool");
+    address poolOwner = _msgSender();
+    uint40 poolIndex = poolOfAuthorizedAddr[poolOwner];
+    require(poolIndex != 0, "The signer does not register a pool");
+    require(poolOwner == ownerOfPool[poolIndex], "Need the pool owner as the signer");
+    poolOfAuthorizedAddr[addr] = poolIndex;
+  }
+
+  /// TODO: @wyf describe this method
+  function removeAuthorizedAddr(address addr) external {
+    address poolOwner = _msgSender();
+    uint40 poolIndex = poolOfAuthorizedAddr[poolOwner];
+    require(poolIndex != 0, "The signer does not register a pool");
+    require(poolOwner == ownerOfPool[poolIndex], "Need the pool owner as the signer");
+    require(poolOfAuthorizedAddr[addr] == poolIndex, "Addr is not authorized for the signer's pool");
+    poolOfAuthorizedAddr[addr] = 0;
+  }
+
   /// @notice Lock funds to match a swap request. This is step 2️⃣ in a swap.
   /// The bonding LP should call this method with the same signature given
   /// by `postSwap`. This method will lock swapping fund on the target chain
