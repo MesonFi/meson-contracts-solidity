a7247de08a1064a21ebf1db0f9d188017880d3fd
Phil Li <qft.gtr@gmail.com>
2022-08-08 17:54:24 -0500

    fix event comments

--- contracts/Pools/IMesonPoolsEvents.sol
+++ contracts/Pools/IMesonPoolsEvents.sol
@@ -3,12 +3,12 @@ pragma solidity 0.8.6;
 
 /// @title MesonPools Interface
 interface IMesonPoolsEvents {
-  /// @notice Event when a swap has been locked.
+  /// @notice Event when a swap was locked.
   /// Emit at the end of `lock()` calls.
   /// @param encodedSwap Encoded swap
   event SwapLocked(uint256 indexed encodedSwap);
 
-  /// @notice Event when a swap has been released.
+  /// @notice Event when a swap was released.
   /// Emit at the end of `release()` calls.
   /// @param encodedSwap Encoded swap
   event SwapReleased(uint256 indexed encodedSwap);
--- contracts/Swap/IMesonSwapEvents.sol
+++ contracts/Swap/IMesonSwapEvents.sol
@@ -3,17 +3,17 @@ pragma solidity 0.8.6;
 
 /// @title MesonSwapEvents Interface
 interface IMesonSwapEvents {
-  /// @notice Event when a swap request has been posted.
+  /// @notice Event when a swap request was posted.
   /// Emit at the end of `postSwap()` calls.
   /// @param encodedSwap Encoded swap
   event SwapPosted(uint256 indexed encodedSwap);
 
-  /// @notice Event when a swap request has been bonded.
+  /// @notice Event when a swap request was bonded.
   /// Emit at the end of `bondSwap()` calls.
   /// @param encodedSwap Encoded swap
   event SwapBonded(uint256 indexed encodedSwap);
 
-  /// @notice Event when a swap request has been cancelled.
+  /// @notice Event when a swap request was cancelled.
   /// Emit at the end of `cancelSwap()` calls.
   /// @param encodedSwap Encoded swap
   event SwapCancelled(uint256 indexed encodedSwap);
