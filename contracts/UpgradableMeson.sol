684663a9e6f0fd0f6772e7564c529e40ae6b5de8
Phil Li <qft.gtr@gmail.com>
2022-09-16 19:32:09 -0500

    create ProxyToMeson

--- contracts/UpgradableMeson.sol
+++ contracts/UpgradableMeson.sol
@@ -2,32 +2,13 @@
 pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "./MesonManager.sol";
 
-import "./Swap/MesonSwap.sol";
-import "./Pools/MesonPools.sol";
-
-contract UpgradableMeson is UUPSUpgradeable, MesonSwap, MesonPools {
-  bool private _initialized;
-  address private _owner;
-
-  function initialize(address[] memory tokens, uint8[] memory indexes) public {
-    require(tokens.length == indexes.length, "Tokens and indexes should have the same length");
-    require(!_initialized, "Contract instance has already been initialized");
-    _initialized = true;
-    _owner = _msgSender();
-    _premiumManager = _msgSender();
-
-    for (uint8 i = 0; i < tokens.length; i++) {
-      _addSupportToken(tokens[i], indexes[i]);
-    }
+contract UpgradableMeson is UUPSUpgradeable, MesonManager {
+  function initialize(address owner, address premiumManager) external initializer {
+    _owner = owner;
+    _premiumManager = premiumManager;
   }
 
-  function _authorizeUpgrade(address newImplementation) internal override {
-    require(_msgSender() == _owner, "Unauthorized");
-  }
-
-  modifier onlyOwner() {
-    require(_owner == _msgSender(), "Caller is not the owner");
-    _;
-  }
+  function _authorizeUpgrade(address) internal override onlyOwner {}
 }
