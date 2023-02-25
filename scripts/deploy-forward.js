fe5186cb6924b6c784452cf9ccb5dd306bd06118
Phil <qft.gtr@gmail.com>
2023-02-24 22:32:58 -0800

    Update deploy-forward.js

--- scripts/deploy-forward.js
+++ scripts/deploy-forward.js
@@ -8,6 +8,7 @@ const { PRIVATE_KEY } = process.env
 
 module.exports = async function deployForwardContract(network) {
   const client = getClient(network)
+  await hre.run('compile')
 
   const wallet = adaptors.getWallet(PRIVATE_KEY, client)
   console.log('Deploying ForwardTokenContract...')
